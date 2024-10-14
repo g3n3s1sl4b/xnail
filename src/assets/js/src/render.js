7bbcf0005f4235c800b6ce31d0e19246436a5e6f
Deface <info@xnail.shop>
2024-10-14 20:58:32 +0200

    UPDATE

--- src/assets/js/src/render.js
+++ src/assets/js/src/render.js
@@ -10,23 +10,19 @@ getProducts();
 
 function renderProducts(productsArray) {
   productsArray.forEach(function (item) {
-    const productHTML = ` <div class="col-md-6 col-xl-4" data-shuffle="item" data-groups=${item.group}>
-              <div class="product-3" >
-              <a class="product-media" href="#">
-                  <span class="badge badge-success badge-pos-right">-${item.discount}%</span>
-                  <img src=${item.img} alt="product">
-                </a>
-                <div class="product-detail">
-                  
-                  <h6><a href="#">${item.name}</a></h6>
-                  <div class="product-price">$${item.price}</div>
-                 
-                </div>
-
-              </div>
-            </div>
-        `;
-    productsContainer.insertAdjacentHTML("beforeend", productHTML);
-
+    const productHTML =` 
+    <div class="col-md-6 col-xl-4" data-shuffle="item" data-groups=${item.group}>
+    <div class="product-3" >
+    <a class="product-media" href="#">
+    <span class="badge badge-success badge-pos-right">-${item.discount}%</span>
+    <img src=${item.img} alt="product">
+    </a>
+    <div class="product-detail">
+    <h6><a href="#">${item.name}</a></h6>
+    <div class="product-price">$${item.price}</div>
+    </div>
+    </div>
+    </div>`;
+  productsContainer.insertAdjacentHTML("beforeend", productHTML);
   });
 }
