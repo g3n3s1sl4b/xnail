87edbee702d2999544104298d93e098855412347
Deface <info@xnail.shop>
2024-10-15 21:35:39 +0200

    UPDATE

--- src/assets/js/src/render.js
+++ src/assets/js/src/render.js
@@ -18,9 +18,34 @@ function renderProducts(productsArray) {
     <img src=${item.img} alt="product">
     </a>
     <div class="product-detail">
-    <h6><a href="#">${item.name}</a></h6>
+    <div class="product-name">$${item.name}</div>
     <div class="product-price">$${item.price}</div>
+    <div class="btn-toolbar justify-content-center" role="toolbar">
+              <div class="btn-group mr-2" role="group" aria-label="First group">
+                <button type="button" class="btn btn-secondary">1</button>
+              </div>
+              <div class="btn-group" role="group" aria-label="Third group">
+                <button type="button" class="btn btn-secondary">8</button>
+              </div>
+            </div>
     </div>
+
+
+
+
+            
+
+
+           
+
+
+
+
+
+
+
+
+    
     </div>
     </div>`;
   productsContainer.insertAdjacentHTML("beforeend", productHTML);
